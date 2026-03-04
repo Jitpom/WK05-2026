de62d236204a5dc98cd89ae20dc3f7d26408a2b2
cmdtrwd <jittrapol.i@psu.ac.th>
2026-03-04 13:28:28 +0700

    Classlist and Toggle()

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("HTML document has been loaded. Yeah!");
 
     const heading = document.querySelector(".heading");
+    const box2 = document.querySelector(".box2");
+
     heading.addEventListener("click", function() {
-        // Add the "active" class to the heading element when clicked
+        // Toggle the "active" class to the heading element when clicked
         heading.classList.toggle("active"); 
+        box2.classList.toggle("active"); // Also toggle the "active" class for box2
     });
 });
