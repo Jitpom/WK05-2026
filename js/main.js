7cd18c9a99c9c86b5e46fab398ef165d70abcd32
cmdtrwd <jittrapol.i@psu.ac.th>
2026-03-04 11:31:51 +0700

    const and querySelector

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("HTML document has been loaded. Yeah!");
+
+    const heading = document.querySelector(".heading");
+    heading.addEventListener("click", function() {
+        alert("You clicked the heading!");
+    });
 });
